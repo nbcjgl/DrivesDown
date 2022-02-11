ea6f4311575ea71c70d90ca2efe723c6d0ac71f3
down-one <hrf@shthey.onmicrosoft.com>
2022-02-12 00:09:23 +0800

    add torrent

--- upfiles.sh
+++ upfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://nyaa.si/download/1473325.torrent
+wget https://nyaa.si/download/1472981.torrent
 aria2c -x 64 --bt-remove-unselected-file=true --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 
-rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/我的文件 - animation_0 的文件/temp/[赞州中学勇者部&LoliHouse] 结城友奈是勇者 大满开之章"
+rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/我的文件 - animation_0 的文件/temp/[喵萌奶茶屋&LoliHouse] 空色Utility / 天空色的多功能杆"
