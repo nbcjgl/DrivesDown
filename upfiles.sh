5c908b406f745c0c5baa1dff61d7cbe3be17dfb4
down-one <80153075+down-one@users.noreply.github.com>
2022-02-11 16:15:55 +0800

    Update upfiles.sh

--- upfiles.sh
+++ upfiles.sh
@@ -6,7 +6,7 @@ aria2c -x 64 --bt-remove-unselected-file=true --seed-time=0 --bt-tracker-connect
 #aria2c -x 64 --bt-remove-unselected-file=true --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 --select-file=601-847 *.torrent
 #rm -rf ./*/*/*
 #rclone move -v --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ onedrive:/二次元/漫画/18+/
-#rm -rf *.torrent
+#rm -rf *.torrent#
 
 rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/我的文件 - animation_0 的文件/temp"
 #rclone copy -v --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ nice:/本子
