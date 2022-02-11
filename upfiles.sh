13b637259ea53cee51b77025aa01d1495aa157c1
down-one <hrf@shthey.onmicrosoft.com>
2022-02-12 00:08:05 +0800

    add torrent

--- upfiles.sh
+++ upfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://nyaa.si/download/1479238.torrent
+wget https://nyaa.si/download/1479109.torrent
 aria2c -x 64 --bt-remove-unselected-file=true --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 
-rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/我的文件 - animation_0 的文件/temp/[风之圣殿&动漫国字幕组&LoliHouse] 超异域公主连结 Re:Dive"
+rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/我的文件 - animation_0 的文件/temp/[悠哈璃羽字幕社&LoliHouse] 国王排名"
