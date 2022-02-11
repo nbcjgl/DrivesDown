0441703f1eb9c9c6138e18a87dc1b0207bb09117
down-one <hrf@shthey.onmicrosoft.com>
2022-02-11 21:33:55 +0800

    add torrent

--- upfiles.sh
+++ upfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://nyaa.si/download/1488443.torrent
+wget https://nyaa.si/download/1488396.torrent
 aria2c -x 64 --bt-remove-unselected-file=true --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 
-rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/我的文件 - animation_0 的文件/temp/[喵萌奶茶屋&LoliHouse] 更衣人偶坠入爱河"
+rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/我的文件 - animation_0 的文件/temp/[百冬练习组&LoliHouse] BanG Dream! 少女乐团派对！☆PICO FEVER！"
