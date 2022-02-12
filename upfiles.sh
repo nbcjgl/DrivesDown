5379fc5b3bfeb63f8aeda3af3c3dbed223836dec
down-one <hrf@shthey.onmicrosoft.com>
2022-02-12 12:10:41 +0800

    add torrent

--- upfiles.sh
+++ upfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://nyaa.si/download/1462607.torrent
+wget https://nyaa.si/download/1462606.torrent
 aria2c -x 64 --bt-remove-unselected-file=true --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 
-rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/我的文件 - animation_0 的文件/temp/[LoliHouse] 因为不是真正的伙伴而被逐出勇者队伍，流落到边境展开慢活人生"
+rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/我的文件 - animation_0 的文件/temp/[LoliHouse] 世界顶尖的暗杀者转生为异世界贵族"
