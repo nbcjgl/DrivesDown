17a73d5d81faac39b6bea6b6c39fbbe6b97463ca
down-one <hrf@shthey.onmicrosoft.com>
2022-02-16 15:30:23 +0800

    add torrent

--- upfiles.sh
+++ upfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://nyaa.si/download/1491608.torrent
+wget https://nyaa.si/download/1491594.torrent
 aria2c -x 64 --bt-remove-unselected-file=true --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 
-rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/我的文件 - animation_0 的文件/temp/Lilith-Raws/[Lilith-Raws] 川尻小玉的懶散生活"
+rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/我的文件 - animation_0 的文件/temp/Lilith-Raws/[Lilith-Raws] 叫我對大哥 WEB版"
