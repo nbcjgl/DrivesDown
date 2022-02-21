54123927e534f18033ed97d6646ebc98a2607072
down-one <hrf@shthey.onmicrosoft.com>
2022-02-21 10:30:15 +0800

    add torrent

--- upfiles.sh
+++ upfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://nyaa.si/download/1493416.torrent
+wget https://nyaa.si/download/1493414.torrent
 aria2c -x 64 --bt-remove-unselected-file=true --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 
 rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/我的文件 - animation_0 的文件/temp/lolihouse/[神滨市立大学勇者部&LoliHouse] 魔法纪录 魔法少女小圆外传 第二季 -觉醒前夜-"
