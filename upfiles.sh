94b15d1835289b4568f913d4a47461a51fe61d5c
down-one <hrf@shthey.onmicrosoft.com>
2022-02-27 16:09:23 +0800

    [桜都字幕组] 女孩的钓鱼慢活

--- upfiles.sh
+++ upfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://nyaa.si/download/1495255.torrent
+wget https://nyaa.si/download/1494561.torrent
 aria2c -x 64 --bt-remove-unselected-file=true --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 
-rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/我的文件 - animation_0 的文件/temp/sakurato/[桜都字幕组] 擅长捉弄的高木同学3"
+rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/我的文件 - animation_0 的文件/temp/sakurato/[桜都字幕组] 女孩的钓鱼慢活"
