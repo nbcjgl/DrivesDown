0a6b3ba600a5d85c7f324b4a9bfa251ec924005b
down-one <hrf@shthey.onmicrosoft.com>
2022-02-28 05:30:08 +0800

    [LoliHouse] 白领羽球部

--- upfiles.sh
+++ upfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://nyaa.si/download/1496384.torrent
+wget https://nyaa.si/download/1496381.torrent
 aria2c -x 64 --bt-remove-unselected-file=true --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 
-rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/我的文件 - animation_0 的文件/temp/lolihouse/[LoliHouse] 最游记 RELOAD -ZEROIN-"
+rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/我的文件 - animation_0 的文件/temp/lolihouse/[LoliHouse] 白领羽球部"
