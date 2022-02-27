1fda1c49027c6aa87a03e30b18a1c566cd0b370b
down-one <hrf@shthey.onmicrosoft.com>
2022-02-27 16:09:38 +0800

    [桜都字幕组] 进击的巨人 The Final Season Part 2

--- upfiles.sh
+++ upfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://nyaa.si/download/1494546.torrent
+wget https://nyaa.si/download/1494504.torrent
 aria2c -x 64 --bt-remove-unselected-file=true --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 
-rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/我的文件 - animation_0 的文件/temp/sakurato/[桜都字幕组] 白金终局"
+rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/我的文件 - animation_0 的文件/temp/sakurato/[桜都字幕组] 进击的巨人 The Final Season Part 2"
