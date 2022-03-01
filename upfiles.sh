75927177833f8a01d5dc376c1d26591ebeeef974
down-one <hrf@shthey.onmicrosoft.com>
2022-03-02 06:30:04 +0800

    [Lilith-Raws] 自稱賢者弟子的賢者

--- upfiles.sh
+++ upfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://nyaa.si/download/1497054.torrent
+wget https://nyaa.si/download/1497217.torrent
 aria2c -x 64 --bt-remove-unselected-file=true --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 
-rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/我的文件 - animation_0 的文件/temp/lolihouse/[喵萌奶茶屋&LoliHouse] 更衣人偶坠入爱河"
+rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/我的文件 - animation_0 的文件/temp/Lilith-Raws/[Lilith-Raws] 自稱賢者弟子的賢者"
