93ea2392fb64af79f642338dea4b18ab0e04f902
down-one <hrf@shthey.onmicrosoft.com>
2022-03-07 00:57:06 +0800

    +++ [HD] YMLW-011 セクハラ上司と出張先のビジネスホテルで相部屋に！爆睡している部下にむりやり中出し4時間

--- upfiles.sh
+++ upfiles.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-<<<<<<< HEAD
-wget https://sukebei.nyaa.si/download/3595995.torrent
-aria2c -x 64 --bt-remove-unselected-file=true --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
-
-rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/我的文件 - animation_0 的文件/temp/offkab/"
-=======
 wget https://sukebei.nyaa.si/download/3595996.torrent
 aria2c -x 64 --bt-remove-unselected-file=true --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 
-rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/我的文件 - animation_0 的文件/"
->>>>>>> 32cc02c823039924dce9116d680155414f5b8fab
+rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/我的文件 - animation_0 的文件/temp/offkab/"
