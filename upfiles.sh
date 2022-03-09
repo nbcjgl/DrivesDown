673a28d60e2370dc42e1b0739a0cc818d2675e12
down-one <hrf@shthey.onmicrosoft.com>
2022-03-10 07:30:06 +0800

    [桜都字幕组] 王子的本命是反派大小姐

--- upfiles.sh
+++ upfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://nyaa.si/download/1500494.torrent
+wget https://nyaa.si/download/1500414.torrent
 aria2c -x 64 --bt-remove-unselected-file=true --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 
-rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/我的文件 - animation_0 的文件/temp/Lilith-Raws/[Lilith-Raws] 東方少年"
+rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/我的文件 - animation_0 的文件/temp/sakurato/[桜都字幕组] 王子的本命是反派大小姐"
