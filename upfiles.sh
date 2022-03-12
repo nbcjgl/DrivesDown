6f90caf90c986e0a01cc8ba8619d5c855d87a514
down-one <hrf@shthey.onmicrosoft.com>
2022-03-13 06:30:05 +0800

    [Lilith-Raws] 怪人開發部的黑井津小姐

--- upfiles.sh
+++ upfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://nyaa.si/download/1501349.torrent
+wget https://nyaa.si/download/1501639.torrent
 aria2c -x 64 --bt-remove-unselected-file=true --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 
-rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/我的文件 - animation_0 的文件/temp/lolihouse/[LoliHouse] 小太郎一个人生活"
+rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/我的文件 - animation_0 的文件/temp/Lilith-Raws/[Lilith-Raws] 怪人開發部的黑井津小姐"
