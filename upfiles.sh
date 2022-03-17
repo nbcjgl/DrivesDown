25a027f4115c3933e7906d65d13bf53e01d72a2a
down-one <hrf@shthey.onmicrosoft.com>
2022-03-18 05:30:06 +0800

    [百冬练习组&LoliHouse] 小小世界

--- upfiles.sh
+++ upfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://nyaa.si/download/1503317.torrent
+wget https://nyaa.si/download/1503305.torrent
 aria2c -x 64 --bt-remove-unselected-file=true --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 
-rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/我的文件 - animation_0 的文件/temp/sakurato/[桜都字幕组] 无职转生～到了异世界就拿出真本事～"
+rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/我的文件 - animation_0 的文件/temp/lolihouse/[百冬练习组&LoliHouse] 小小世界"
